488181d9c3b6f714cee875d3f8d6454e6b5ec107
Dan Le <tle57@gmu.edu>
2022-09-12 19:13:07 -0400

    update grid layout

--- src/HomePage/WorkoutCard.tsx
+++ src/HomePage/WorkoutCard.tsx
@@ -1,3 +1,10 @@
-export default function WorkoutCard() {
-  return <div className="h-[90%] w-[70%] rounded-3xl bg-gray-900"></div>;
+export default function WorkoutCard({ workoutName }: { workoutName: string }) {
+  return (
+    <div className="flex h-[85%] w-[90%] items-center justify-center rounded-3xl bg-black shadow-2xl">
+      <div className="select-none font-serif text-3xl text-white">
+        {workoutName}
+      </div>
+      <div></div>
+    </div>
+  );
 }
--- src/HomePage/WorkoutContainer.tsx
+++ src/HomePage/WorkoutContainer.tsx
@@ -2,14 +2,16 @@ import WorkoutCard from "./WorkoutCard";
 
 export default function WorkoutContainer() {
   return (
-    <div className="absolute bottom-0 z-0 flex h-[85%] w-full items-center justify-center">
-      <div className="absolute z-10 grid h-[95%] w-[95%] grid-cols-3 grid-rows-2 items-center justify-items-center rounded-xl bg-slate-50 shadow-2xl">
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
+    <div className="absolute bottom-0 z-0 flex h-[88%] w-full items-center justify-center">
+      <div className="absolute z-10 grid h-[95%] w-[90%] grid-cols-5 grid-rows-3 items-center justify-items-center rounded-xl bg-slate-100 shadow-2xl">
+        <WorkoutCard workoutName="push" />
+        <WorkoutCard workoutName="pull" />
+        <WorkoutCard workoutName="legs" />
+        <WorkoutCard workoutName="push" />
+        <WorkoutCard workoutName="pull" />
+        <WorkoutCard workoutName="legs" />
+        <WorkoutCard workoutName="push" />
+        <WorkoutCard workoutName="pull" />
       </div>
     </div>
   );
