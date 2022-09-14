aeb8784f6afe047f5e672c96a4865d9e960ee68e
Dan Le <tle57@gmu.edu>
2022-09-14 14:45:43 -0400

    yes

--- src/HomePage/WorkoutCard.tsx
+++ src/HomePage/WorkoutCard.tsx
@@ -1,6 +1,6 @@
 export default function WorkoutCard({ workoutName }: { workoutName: string }) {
   return (
-    <div className="flex h-[85%] w-[90%] items-center justify-center rounded-3xl bg-black shadow-2xl">
+    <div className=" flex h-[90%] w-[70%] items-center justify-center rounded-3xl bg-black shadow-2xl">
       <div className="select-none font-serif text-3xl text-white">
         {workoutName}
       </div>
