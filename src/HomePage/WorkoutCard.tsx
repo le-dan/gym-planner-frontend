24a173017a3db9c71202663fa638e8fbf8956707
Dan Le <tle57@gmu.edu>
2022-09-15 01:52:48 -0400

    update colors

--- src/HomePage/WorkoutCard.tsx
+++ src/HomePage/WorkoutCard.tsx
@@ -5,14 +5,12 @@ export default function WorkoutCard({ workoutName }: { workoutName: string }) {
     // Workout Card Container
 
     // have onclick on container
-    <div className="flex h-[95%] w-[85%] items-center justify-center rounded-3xl bg-slate-100 shadow-lg">
+    <div className="flex h-[95%] w-[85%] items-center justify-center rounded-3xl bg-slate-100 shadow-xl">
       <motion.div
-        whileHover={{ scale: 1.1 }}
-        className="flex h-[80%] w-[75%] cursor-pointer items-center justify-center rounded-3xl bg-zinc-800 shadow-xl"
+        whileHover={{ scale: 1.05 }}
+        className="flex h-[80%] w-[75%] cursor-pointer items-center justify-center rounded-3xl bg-zinc-100 shadow-xl"
       >
-        <div className="select-none font-serif text-3xl text-white">
-          {workoutName}
-        </div>
+        <div className="select-none font-serif text-3xl">{workoutName}</div>
         <div></div>
       </motion.div>
     </div>
