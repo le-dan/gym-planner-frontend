021ab325a77be34a611098e4b0214f093e2e13d2
Dan Le <tle57@gmu.edu>
2022-09-15 01:48:35 -0400

    change sizing of grid layout

--- src/HomePage/WorkoutCard.tsx
+++ src/HomePage/WorkoutCard.tsx
@@ -1,10 +1,20 @@
+import { motion } from "framer-motion";
+
 export default function WorkoutCard({ workoutName }: { workoutName: string }) {
   return (
-    <div className=" flex h-[95%] w-[75%] items-center justify-center rounded-3xl bg-zinc-800 shadow-2xl">
-      <div className="select-none font-serif text-3xl text-white">
-        {workoutName}
-      </div>
-      <div></div>
+    // Workout Card Container
+
+    // have onclick on container
+    <div className="flex h-[95%] w-[85%] items-center justify-center rounded-3xl bg-slate-100 shadow-lg">
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        className="flex h-[80%] w-[75%] cursor-pointer items-center justify-center rounded-3xl bg-zinc-800 shadow-xl"
+      >
+        <div className="select-none font-serif text-3xl text-white">
+          {workoutName}
+        </div>
+        <div></div>
+      </motion.div>
     </div>
   );
 }
--- src/HomePage/WorkoutContainer.tsx
+++ src/HomePage/WorkoutContainer.tsx
@@ -6,15 +6,15 @@ export default function WorkoutContainer() {
     <div className="absolute bottom-0 z-0 flex h-[88%] w-full items-center justify-center">
       {/* Background card */}
       <div className="absolute bottom-0 flex h-[95%] w-[95%] items-center justify-center rounded-t-3xl bg-slate-100 shadow-2xl">
-        <div className="absolute z-10 grid h-[90%] w-[98%] grid-cols-5 grid-rows-3 items-center justify-items-center gap-y-5 ">
+        <div className="absolute z-10 grid h-[90%] w-[98%] grid-cols-4 grid-rows-2 items-center justify-items-center gap-y-5 ">
           <WorkoutCard workoutName="push" />
           <WorkoutCard workoutName="pull" />
           <WorkoutCard workoutName="legs" />
           <WorkoutCard workoutName="push" />
           <WorkoutCard workoutName="pull" />
           <WorkoutCard workoutName="legs" />
-          <WorkoutCard workoutName="push" />
           <WorkoutCard workoutName="pull" />
+          <WorkoutCard workoutName="legs" />
         </div>
       </div>
     </div>
