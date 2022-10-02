c20d731dee12408cab3c7ae5f2b403ce2a0ddd21
Dan Le <tle57@gmu.edu>
2022-10-02 13:39:26 -0400

    move file structure

--- TODO.md
+++ TODO.md
@@ -1,10 +1,19 @@
+** make ability to remove workout 
 
 
 ** make post to add new workout to database
 
-- learn how to parse data
 
-- add github/linked icon to bottem left corner
+
+
+- learn how to parse data
+- add github/linked icon to bottom left corner
 
 colors for name:
 text-gray-500
+
+color for inner workout card:
+bg-zinc-100
+
+background workout container card:
+bg-slate-100
--- src/HomePage/WorkoutPage/AddWorkoutCard.tsx
+++ src/HomePage/WorkoutPage/AddWorkoutCard.tsx
@@ -5,13 +5,13 @@ export default function AddWorkoutCard({ setTrigger }: { setTrigger: any }) {
   return (
     <motion.div
       whileHover={{ scale: 1.05 }}
-      className="flex h-[55%] w-[45%] items-center justify-center rounded-3xl bg-slate-100 shadow-xl"
+      className="workoutCardOuter flex h-[55%] w-[45%] items-center justify-center rounded-3xl shadow-xl"
     >
       <motion.div
         onClick={() => setTrigger(true)}
-        className="flex h-[80%] w-[75%] cursor-pointer items-center justify-center rounded-3xl bg-zinc-100 shadow-xl"
+        className="workoutCardInner flex h-[80%] w-[75%] cursor-pointer items-center justify-center rounded-3xl shadow-xl"
       >
-        <div className="select-none font-serif text-4xl">+</div>
+        <div className="select-none text-4xl font-extralight">+</div>
       </motion.div>
     </motion.div>
   );
